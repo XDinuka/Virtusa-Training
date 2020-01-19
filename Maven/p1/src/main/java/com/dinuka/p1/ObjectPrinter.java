0b8cddbc8f2aaef8984d42133eb2e47c7b3affb0
Dinuka Karunananda <dinuka.nirosh@gmail.com>
2020-01-19 22:40:17 +0530

    Initial Commit

---
+++ Maven/p1/src/main/java/com/dinuka/p1/ObjectPrinter.java
@@ -0,0 +1,7 @@
+package com.dinuka.p1;
+public class ObjectPrinter {
+
+    public void print(Object object){
+        System.out.println(object.toString());
+    }
+}
---
+++ Maven/p2/src/main/java/com/dinuka/p2/Run.java
@@ -0,0 +1,12 @@
+package com.dinuka.p2;
+//import com.dinuka.p1.ObjectPrinter;
+public class Run{
+	
+	
+	
+	   public static void main(String[] args) {
+        System.out.println("HI");
+  //     new ObjectPrinter().print("Hello Maven");
+    }
+	
+}
